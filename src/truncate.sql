51251e151857f8309bb4a082e45278ae359c11a7
Vsev0l0d <vsevolod628@gmail.com>
2021-11-14 20:19:30 +0300

    truncate

---
+++ src/truncate.sql
@@ -0,0 +1,3 @@
+truncate event_personage_type, clothing, weapon,
+    personage_type, thing_control, location, fairy_personage, personage,
+    block_list, event, invitation, prise restart identity cascade;
