6f89c14812572d722a02c30c9484234883a10b5e
Nadya <kirillova202001@yandex.ru>
2021-11-15 11:11:53 +0300

    update indexes.sql

--- src/indexes.sql
+++ src/indexes.sql
@@ -1,3 +1,3 @@
-create index invitation_index on invitation (id);
-create index event_index on event (id);
+create index invitation_index on invitation using hash (id);
+create index event_index on event using hash (id);
 create index location_index on location using hash (id);
